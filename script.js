25efc897f4fc806309c1264e361331f8f3d3f958
Yari-Carelli <yaricarelli@hotmail.it>
2023-05-04 17:21:06 +0000

    Add scroll to top function

--- script.js
+++ script.js
@@ -62,3 +62,20 @@ $(window).scroll(function() {
         });
     } 
   });
+  
+  
+    /* ***** Scroll to Top ***** */ 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 300) {        
+            $('.to-top').fadeIn(200);    
+        } else {
+            $('.to-top').fadeOut(200);   
+        }
+    });
+    $('.to-top').click(function() {      
+        $('.body,html').animate({
+            scrollTop : 0                       
+        }, 500);
+    });
+  
+  })
