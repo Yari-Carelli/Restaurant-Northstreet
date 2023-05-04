8f1ab16f77c3b09277cd801f4669fc54801f92b7
Yari-Carelli <yaricarelli@hotmail.it>
2023-05-04 17:19:53 +0000

    Add smooth scrolling function

--- script.js
+++ script.js
@@ -41,4 +41,24 @@ $(window).scroll(function() {
         $(this).addClass("slide");
       }
     });
+  }); 
+  
+  
+  
+  
+  /* ***** Smooth Scrolling  ***** */
+  $(document).ready(function(){ 
+    $(".navbar a, #service a").on('click', function(event) {
+  
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+  
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+  
+        window.location.hash = hash;
+        });
+    } 
   });
