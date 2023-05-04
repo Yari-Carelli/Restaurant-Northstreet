57b32675d1aad2a7ddd3ffe6684d6a5688ee1db0
Yari-Carelli <yaricarelli@hotmail.it>
2023-05-04 17:30:39 +0000

    Review script.js

--- script.js
+++ script.js
@@ -2,37 +2,40 @@
 $(function(){
 
     var nav = $('.navbar'),
-        doc = $(document),
-        win = $(window);
-
+      doc = $(document),
+      win = $(window);
+  
     win.scroll(function() {
-
-        if (doc.scrollTop() > 80) {
-            nav.addClass('scrolled');
-        } else {
-            nav.removeClass('scrolled');
-        }
-
+  
+      if (doc.scrollTop() > 80) {
+        nav.addClass('scrolled');
+      } else {
+        nav.removeClass('scrolled');
+      }
+  
     });
-
+  
     win.scroll();
-
-});
-
-
-/* ***** Btn More-Less ***** */
-$("#more").click(function(){
-    var $this = $(this);
-    $this.toggleClass('more');
-    if($this.hasClass('more')){
-        $this.text('More');         
-    } else {
-        $this.text('Less');
-    }
-});
-
-/* ***** Slideanim  ***** */
-$(window).scroll(function() {
+  
+  });
+  
+  
+  /* ***** Btn More-Less ***** */
+  $("#more").click(function(){
+      var $this = $(this);
+      $this.toggleClass('more');
+      if($this.hasClass('more')){
+          $this.text('More');         
+      } else {
+          $this.text('Less');
+      }
+  });
+  
+  
+  
+  
+  /* ***** Slideanim  ***** */
+   $(window).scroll(function() {
     $(".slideanim").each(function(){
       var pos = $(this).offset().top;
   
